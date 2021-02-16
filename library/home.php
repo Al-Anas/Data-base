77fe169677af3c7ea8739e210a3715baf407d2fa
Al-Anas <41803446+Al-Anas@users.noreply.github.com>
2021-02-16 21:48:09 +0600

    Add files via upload

---
+++ library/home.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Blog Home - Start Bootstrap Template</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/blog-home.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">HSTU LIBRARY</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="home.php">Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="student.php">Student</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="book.php">Book</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="librarian.php">Librarian</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Page Content -->
+  <div style="width:80%;margin:0 auto;" >
+    <div class="card" style="margin-top:100px; margin-bottom:150px;">
+  <div class="card-header">
+    
+  </div>
+  <div class="card-body">
+   <h1 class="display-1 text-center">HSTU LIBRARY</h1>
+  </div>
+  
+
+
+    
+  </div>
+</div>
+
+
+
+
+  </div>
+
+       
+      
+
+        
+       
+
+  <!-- Footer -->
+  <div style="margin-top:200px">
+  <footer class="py-5 bg-dark  ">
+    <div class="container ">
+      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2020</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+  </div>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
